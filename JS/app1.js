db1336e07263a37957a14a1e83f6e16496cb4fda
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2024-12-25 23:22:19 +0530

    Object creation

---
+++ JS/app1.js
@@ -0,0 +1,53 @@
+ const student={
+    name:"ayush",
+    age:24,
+    marks:90,
+};
+const post={
+    username:"@ayush",
+    content:"this is my #first post",
+    likes:150,
+    reposts:20,
+    tags:["@baigangang","@coding","@javascript"],
+};
+const obj={
+    1 :"a",
+    2 :"b",
+    3 :"c",
+    null :"d",
+    undefined :"e"
+
+};
+// Nesting Objects
+const classInfo={
+    aman:{
+        grade:"A+",
+        city:"Delhi",
+    },
+    ayush:{
+        grade:"O",
+        city:"Mumbai",
+    },
+    shradha:{
+        grade:"B+",
+        city:"Bangalore",
+    }
+};
+// Array of Objects
+const myclasss=[
+    {
+        name:"aman",
+        age:24,
+        marks:90,
+    },
+    {
+        name:"ayush",
+        age:24,
+        marks:90,
+    },
+    {
+        name:"shradha",
+        age:24,
+        marks:90,
+    },
+];
