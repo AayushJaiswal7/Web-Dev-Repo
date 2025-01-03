c206dc82c86d7e2828ee4263900ef54002230862
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-01-03 23:40:09 +0530

    DOM

---
+++ JS/app3.js
@@ -0,0 +1,25 @@
+// let smallimages=document.getElementsByClassName("oldImg");
+
+// for (let i = 0; i <smallimages.length; i++) {
+//     smallimages[i].src="spiderman_img.png";
+//     console.log(`value of image no.${i} is changed.`);
+    
+    
+// }
+console.dir(document.querySelector("h1"));
+
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelector("div a"));
+
+console.dir(document.querySelectorAll("div a"));
+
+let links=document.querySelectorAll(".box a");
+// for (let i = 0; i < links.length; i++) {
+//     links[i].style.color="purple";
+    
+// }
+for(link of links){
+    link.style.color="yellow";
+}
