bd9351f574b5a049430336914a486cdce5779320
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-01-18 00:33:33 +0530

    Practice ques

--- JS/app3.js
+++ JS/app3.js
@@ -47,3 +47,15 @@ let h3=document.createElement("h3");
 h3.innerText="Hey Im blue h3";
 document.querySelector('body').append(h3);
 h3.style.color="blue";
+
+let div=document.createElement("div");
+let h1=document.createElement("h1");
+let para2=document.createElement("p");
+ 
+h1.innerText="I'm in a div";
+para2.innerText="Me Too";
+
+div.append(h1);
+div.append(para2);
+div.classList.add("box");
+document.querySelector('body').prepend(div);
