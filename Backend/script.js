13cd54d1f79ae698ae570b97ab7308ca5e218756
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-01-28 16:25:40 +0530

    Backend with Node(Add fruit modules and math utility functions)

---
+++ Backend/script.js
@@ -0,0 +1,21 @@
+const math=require("./math");
+console.log(math);
+
+const info=require("./Fruits");
+console.log(info);
+
+// let n=5;
+// for (let index = 0; index < n; index++) {
+//     console.log("hello,",index);
+    
+    
+// }
+// console.log("bye!");
+// console.log(process.argv);
+
+// let args=process.argv;
+
+// for(let i = 2;i<args.length;i++){
+// console.log("hello to",args[i]);
+
+// }
