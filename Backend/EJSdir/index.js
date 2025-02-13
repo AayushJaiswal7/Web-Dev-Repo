d8e4de2bf768aafadb633bdb4a94a433bd5da146
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-02-13 12:04:57 +0530

    Set EJS view engine and configure views directory

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const app=express();
 const port=8080;
+const path=require("path");
 
 app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
     
