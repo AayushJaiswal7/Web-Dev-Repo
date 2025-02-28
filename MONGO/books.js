f7ed5c41492e7fe39b388f5714ecc2b69516a345
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-02-28 15:16:11 +0530

    Enhance book schema with validation and new fields; update sample book data

--- MONGO/books.js
+++ MONGO/books.js
@@ -10,23 +10,40 @@ const bookSchema = mongoose.Schema({
     title: {
         type: String,
         required: true,
+        maxlength:20,
     },
     author: {
         type: String,
     },
     price: {
         type: Number,
+        min:[1,"price is too low for selling"],
     },
     discount:{
         type: Number,
         default:0,
-    }
+    },
+    category:{
+        type:String,
+        enum:["fiction","non-fiction"]
+    },
+    genre:[String]
 });
 const Book = mongoose.model("Book", bookSchema);
+
+Book.findByIdAndUpdate("67c05f5c9a478a5138a2c8f4",{price:-100},{runValidators:true}).then((res)=>{
+    console.log(res);
+
+}).catch((err)=>{
+    console.log(err.errors);
+    
+});
 let book1=new Book({
-    title:"Gone Girl",
+    title:"Marvel comics",
     // author:"RD ",
-    price:100,
+    price:20000,
+    // category:"fiction",
+    genre:["comic","superheros","adventure"],
 });
 
 book1.save().then((res)=>{
