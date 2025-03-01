33d8e03205cd0cddfd7452644da6f52b6cd9a102
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-03-01 15:39:05 +0530

    Add chat editing and deletion functionality; implement method override and update routes

---
+++ Home Hive/app.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+main()
+    .then(()=>{
+    console.log("connected to DB");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/homehive');
+    }
+
+
+app.get("/",(req,res)=>{
+    res.send("hi, i am root");
+})
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+    
+})
--- MONGO3/index.js
+++ MONGO3/index.js
@@ -3,9 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride=require("method-override");
+
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
-
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
@@ -63,7 +64,7 @@ newChat
 // })
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", { chats });
 
 })
@@ -71,6 +72,33 @@ app.get("/chats", async (req, res) => {
 app.get("/chats/new", (req, res) => {
     res.render("new.ejs");
 });
+//Edit Route
+app.get("/chats/:id/edit",async(req,res)=>{
+    let{id}=req.params;
+    let chat=await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+})
+//Update Route
+app.put("/chats/:id",async(req,res)=>{
+    let{id}=req.params;
+    let {msg:newMsg}=req.body;
+        
+    let updatedChat=await Chat.findByIdAndUpdate(
+        id,
+        {msg:newMsg},
+        {runValidators:true,new:true}
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+    
+})
+app.delete("/chats/:id",async(req,res)=>{
+    let{id}=req.params;
+    let deletedChat=await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats")
+
+})
 app.get("/", (req, res) => {
     res.send("root is working");
 });
