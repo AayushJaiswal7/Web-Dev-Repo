61ab9d5d7eb43fe01979dc3e4f16b64f14d9d429
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-03-06 00:47:05 +0530

    Add listing management features; implement Mongoose model, views, and routes for creating and displaying listings

--- Home Hive/app.js
+++ Home Hive/app.js
@@ -1,23 +1,67 @@
-const express= require("express");
-const app=express();
-const mongoose=require("mongoose");
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
 main()
-    .then(()=>{
-    console.log("connected to DB");
+    .then(() => {
+        console.log("connected to DB");
     })
-    .catch(err => console.log(err));
+    .catch((err) => { console.log(err); });
+
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/homehive');
-    }
+    await mongoose.connect("mongodb://127.0.0.1:27017/homehive");
+}
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true,}));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hi, i am root");
+});
+
+//index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+});
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
 })
 
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080");
+//show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+//create route
+app.post("/listings",async(req,res)=>{
+    // let{title,description}
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
     
+});
+
+// app.get("/testListing",async(req,res)=>{
+//     let sampleLisiting=new Listing({
+//         title:"My New Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Calangute,Goa",
+//         country:"India",
+//     });
+//     await sampleLisiting.save();
+//     console.log("sample was saved");
+//     res.send('successful testing');
+
+// });
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+
 })
