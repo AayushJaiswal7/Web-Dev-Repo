8d71546767ed1092115e6ddbe284aa6a0fdb2761
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-03-07 03:41:29 +0530

    Add listing editing and deletion functionality; implement routes and views for editing and deleting listings

--- Home Hive/app.js
+++ Home Hive/app.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+
 main()
     .then(() => {
         console.log("connected to DB");
@@ -17,6 +20,8 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true,}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
 
 app.get("/", (req, res) => {
     res.send("hi, i am root");
@@ -45,6 +50,27 @@ app.post("/listings",async(req,res)=>{
     await newListing.save();
     res.redirect("/listings");
     
+});
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+//update route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+});
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+    
+
 });
 
 // app.get("/testListing",async(req,res)=>{
