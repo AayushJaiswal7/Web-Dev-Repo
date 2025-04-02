bea10edc6cf81a308ab53af50ab62c3e790b4b18
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-04-02 19:06:43 +0530

    Add navigation and footer components; update layout and styles for improved UI

--- Home Hive/models/listing.js
+++ Home Hive/models/listing.js
@@ -1,25 +1,22 @@
-const mongoose =require("mongoose");
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const listingSchema=new Schema({
-  title: {
+const listingSchema = new Schema({
+    title: {
         type: String,
-        required:true,
+        required: true,
     },
     description: String,
-    image:{
-        filename: String,
-        url:{
-            type:String,
-            default:"https://unsplash.com/photos/golden-gate-bridge-during-daytime-gZXx8lKAb7Y",
-         set:(v)=>
-        v===""?"https://unsplash.com/photos/golden-gate-bridge-during-daytime-gZXx8lKAb7Y":v,
-        }
-        
+    image: {
+    type: String,
+    set: (v) =>
+            v === "" ? "https://unsplash.com/photos/golden-gate-bridge-during-daytime-gZXx8lKAb7Y" : v,
+
+
     },
     price: Number,
     location: String,
     country: String,
 });
-const Listing=mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
