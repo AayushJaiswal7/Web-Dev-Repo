5154085f97bf08dec59f660983a333017c2ad65c
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-04-03 00:46:41 +0530

    Refactor listing model and update views: enhance image handling, improve card layout, and apply new font styles

--- Home Hive/models/listing.js
+++ Home Hive/models/listing.js
@@ -2,17 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
+    // title: {
+    //     type: String,
+    //     required: true,
+    //   },
+    //   description: String,
+    //   image: {
+    //       filename: String,
+    //       url: String,
+    //   },
+    //   price: Number,
+    //   location: String,
+    //   country: String,
     title: {
         type: String,
         required: true,
     },
     description: String,
     image: {
-    type: String,
-    set: (v) =>
-            v === "" ? "https://unsplash.com/photos/golden-gate-bridge-during-daytime-gZXx8lKAb7Y" : v,
-
-
+        filename: { type: String, default: "listingimage" },
+        url: {
+            type: String,
+            default: "https://unsplash.com/photos/golden-gate-bridge-during-daytime-gZXx8lKAb7Y",
+        },
     },
     price: Number,
     location: String,
