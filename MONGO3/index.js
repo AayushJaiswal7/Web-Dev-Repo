a8b31488244ba65f38850186be0e9c1a6e968efd
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-04-11 11:25:59 +0530

    Refactor chat routes and error handling: update chat creation to use async/await, add error handling middleware, and implement ExpressError class for better error management.

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
-const methodOverride=require("method-override");
+const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError");
+const { dir } = require("console");
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +17,7 @@ main().then(() => {
 
 }).catch((err) => { console.log(err) });
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 };
 
 // let chat1=new Chat({
@@ -26,27 +28,24 @@ async function main() {
 // })
 
 //Create Route
-app.post("/chats", (req, res) => {
-    let { from, to, msg } = req.body;
-    let newChat = new Chat({
-        from: from,
-        to: to,
-        msg: msg,
-        created_at: new Date(),
-    });
-newChat
-    .save()
-    .then((res) => {
-        console.log("chat was saved");
+app.post("/chats", async (req, res, next) => {
+    try {
+        let { from, to, msg } = req.body;
+        let newChat = new Chat({
+            from: from,
+            to: to,
+            msg: msg,
+            created_at: new Date(),
+        });
 
-    }).catch((err) => {
-        console.log(err);
-
-    });
-    res.redirect("/chats");
-
-
+        await newChat.save();
+        res.redirect("/chats");
+    } catch (err) {
+        next(err);
+    }
 });
+
+
 // Chat.deleteMany({from:"neha"}).then((res)=>{
 //     console.log("Done");
 
@@ -70,31 +69,48 @@ app.get("/chats", async (req, res) => {
 })
 //New route
 app.get("/chats/new", (req, res) => {
+    // throw new ExpressError(404,"Page not found");
     res.render("new.ejs");
 });
+
+function asyncWrap(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((err) => next(err));
+    }
+}
+// New-show route
+app.get("/chats/:id", asyncWrap(async (req, res, next) => {
+        let { id } = req.params;
+        let chat = await Chat.findById(id);
+        if (!chat) {
+            next(new ExpressError(404, "chat not found"));
+        }
+        res.render("edit.ejs", { chat });
+
+    }));
 //Edit Route
-app.get("/chats/:id/edit",async(req,res)=>{
-    let{id}=req.params;
-    let chat=await Chat.findById(id);
-    res.render("edit.ejs",{chat});
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
 })
 //Update Route
-app.put("/chats/:id",async(req,res)=>{
-    let{id}=req.params;
-    let {msg:newMsg}=req.body;
-        
-    let updatedChat=await Chat.findByIdAndUpdate(
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+
+    let updatedChat = await Chat.findByIdAndUpdate(
         id,
-        {msg:newMsg},
-        {runValidators:true,new:true}
+        { msg: newMsg },
+        { runValidators: true, new: true }
     );
     console.log(updatedChat);
     res.redirect("/chats");
-    
+
 })
-app.delete("/chats/:id",async(req,res)=>{
-    let{id}=req.params;
-    let deletedChat=await Chat.findByIdAndDelete(id);
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
     console.log(deletedChat);
     res.redirect("/chats")
 
@@ -102,7 +118,18 @@ app.delete("/chats/:id",async(req,res)=>{
 app.get("/", (req, res) => {
     res.send("root is working");
 });
-
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name==="ValidationError"){
+        console.dir(err);
+    }
+    next(err);   
+})
+//Error Handling middleware
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Some error Occurred" } = err;
+    res.status(status).send(message);
+});
 app.listen(8080, () => {
     console.log("server is listening on port 8080");
 
