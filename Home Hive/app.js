66f019d87fef95aa404d5c90f4bcf5da01103fe3
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-07-07 18:13:20 +0530

    Refactor routing and validation: separate listing and review routes into dedicated files, implement validation middleware for reviews, and enhance listing deletion to remove associated reviews.

--- Home Hive/app.js
+++ Home Hive/app.js
@@ -10,6 +10,10 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema,reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
+const  listing = require("./routes/listing.js");
+const  review = require("./routes/review.js");
+
+
 main()
     .then(() => {
         console.log("connected to DB");
@@ -28,104 +32,15 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 
-const validateListing=(req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-};
+
 app.get("/", (req, res) => {
     res.send("hi, i am root");
 });
-const validateReview=(req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-};
 
-//index route
-app.get("/listings", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-}));
-//new route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-})
-
-//show route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", { listing });
-}));
-//create route
-app.post("/listings",
-    validateListing,
-     wrapAsync(async (req, res,next) => {
-    // let{title,description}
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
-);
-//edit route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
-//update route
-app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-        throw new ExpressError(400, "Send valid data!");
-    }
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect("/listings");
-}));
-//delete route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
+app.use("/listings",listing);
+app.use("/listings/:id/reviews",review);
 
 
-}));
-// post Reviews route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
-    
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-
-    
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// delete review route->pull operator
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let{id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-}))
 // app.get("/testListing",async(req,res)=>{
 //     let sampleLisiting=new Listing({
 //         title:"My New Villa",
