aba6e8030a435ac83d0ce2b4f4ac6a590844a57a
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-07-11 09:54:20 +0530

    Implement user authentication: add passport.js for user login/signup, create user model and routes, and integrate flash messaging for notifications.

--- Home Hive/app.js
+++ Home Hive/app.js
@@ -11,8 +11,12 @@ const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
-const listing = require("./routes/listing.js");
-const review = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+const passport=require("passport");
+const localStrategy=require("passport-local");
+const User=require("./models/user.js");
 
 
 main()
@@ -45,18 +49,29 @@ const sessionOptions = {
 }
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success")
     res.locals.error=req.flash("error")
     next();
 });
+
+
 app.get("/", (req, res) => {
     res.send("hi, i am root");
 });
 
-app.use("/listings", listing);
-app.use("/listings/:id/reviews", review);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter)
 
 
 // app.get("/testListing",async(req,res)=>{
